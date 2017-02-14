ed2638fbf3177b72dda1a9df7012b1b6f26f128e
Jeff Scott Brown <brownj@ociweb.com>
2017-02-14 10:57:13 -0600

    Fix failing test

--- src/integration-test/groovy/com/epseelon/TodoControllerSpec.groovy
+++ src/integration-test/groovy/com/epseelon/TodoControllerSpec.groovy
@@ -1,9 +1,10 @@
 package com.epseelon
 
 import geb.spock.GebSpec
-import grails.plugins.rest.client.RestBuilder
+import grails.http.client.RxHttpClientBuilder
 import grails.test.mixin.integration.Integration
 import grails.transaction.Transactional
+import spock.lang.Shared
 
 /**
  * Created by sarbogast on 14/02/2017.
@@ -11,10 +12,16 @@ import grails.transaction.Transactional
 @Integration
 @Transactional
 class TodoControllerSpec extends GebSpec {
+
+    @Shared RxHttpClientBuilder rest = new RxHttpClientBuilder()
+
     void "test get todos"() {
-        expect:
-        def resp = new RestBuilder().get("$baseUrl/todo")
-        resp.json.size() == 3
-        ['TODO', 'IN_PROGRESS', 'DONE'].contains(resp.json[0].status)
+        when:
+        def resp = rest.get("http://localhost:${serverPort}/todo").toBlocking().first()
+        def jsonResponse = resp.toJson().toBlocking().first()
+
+        then:
+        jsonResponse.size() == 3
+        ['TODO', 'IN_PROGRESS', 'DONE'].contains(jsonResponse[0].status)
     }
 }
